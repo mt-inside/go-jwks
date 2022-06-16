ba7f877a38ebb95c6aaeb115e25f3c55c6b30e18
Matt Turner <git@mt165.co.uk>
2022-06-16 18:03:30 +0100

    Support for outputting private keys

--- main.go
+++ main.go
@@ -29,6 +29,10 @@ type printableRsaPublicKey rsa.PublicKey
 type printableEcdsaPublicKey ecdsa.PublicKey
 type printableEd25519PublicKey ed25519.PublicKey
 
+type printableRsaPrivateKey rsa.PrivateKey
+type printableEcdsaPrivateKey ecdsa.PrivateKey
+type printableEd25519PrivateKey ed25519.PrivateKey
+
 func padEven(n string) string {
 	if len(n)%2 == 1 {
 		return "0" + n
@@ -44,7 +48,6 @@ func main() {
 	}
 	flags.Parse(&opts)
 
-	// TODO: read keys(s) from stdin
 	bytes, err := io.ReadAll(os.Stdin)
 	if err != nil {
 		panic(err)
@@ -60,22 +63,27 @@ func main() {
 		bytes = rest
 	}
 
+	parseKey := pubKey
+	if opts.Private {
+		parseKey = privKey
+	}
+
 	if opts.Singleton {
 		if len(blocks) != 1 {
 			panic("--singleton requires input PEM containing precisely one key")
 		}
-		op(process(blocks[0]))
+		op(parseKey(blocks[0]))
 		os.Exit(0)
 	}
 
 	var keys jwks
 	for _, block := range blocks {
-		keys.Keys = append(keys.Keys, process(block))
+		keys.Keys = append(keys.Keys, parseKey(block))
 	}
 	op(keys)
 }
 
-func process(block *pem.Block) jwk {
+func pubKey(block *pem.Block) jwk {
 
 	var key crypto.PublicKey
 	if pubKey, err := x509.ParsePKIXPublicKey(block.Bytes); err == nil {
@@ -111,6 +119,40 @@ func process(block *pem.Block) jwk {
 	return printableKey
 }
 
+func privKey(block *pem.Block) jwk {
+
+	var key crypto.PrivateKey // alias: any
+	if _, err := x509.ParsePKIXPublicKey(block.Bytes); err == nil {
+		panic("Need a private key; got a public")
+	} else if _, err := x509.ParseCertificate(block.Bytes); err == nil {
+		panic("Need a private key; got a cert")
+	} else if privKey, err := x509.ParsePKCS1PrivateKey(block.Bytes); err == nil { // RSA only; type *rsa.PrivateKey
+		key = privKey
+	} else if privKey, _ := x509.ParsePKCS8PrivateKey(block.Bytes); err == nil { // RSA, ECDSA, Ed25519; type any, however: https://pkg.go.dev/crypto#PrivateKey
+		key = privKey
+	} else if privKey, err := x509.ParseECPrivateKey(block.Bytes); err == nil { // ECDSA only; type *ecdsa.PrivateKey
+		key = privKey
+	} else {
+		panic("input PEM does not encode a private key")
+	}
+
+	// TODO: do generics help??
+	var printableKey jwk
+	switch typedKey := key.(type) {
+	case *rsa.PrivateKey:
+		printableKey = (*printableRsaPrivateKey)(typedKey)
+	case *ecdsa.PrivateKey:
+		printableKey = (*printableEcdsaPrivateKey)(typedKey)
+	case ed25519.PrivateKey: // Not a pointer *shrug*
+		printableKey = (printableEd25519PrivateKey)(typedKey)
+		panic("JWK does not support Ed25519")
+	default:
+		panic(fmt.Sprintf("Unknown key type: %T", key))
+	}
+
+	return printableKey
+}
+
 func (k *printableRsaPublicKey) MarshalJSON() ([]byte, error) {
 	bufE := make([]byte, 4)
 	binary.LittleEndian.PutUint32(bufE, uint32(k.E))
@@ -120,12 +162,35 @@ func (k *printableRsaPublicKey) MarshalJSON() ([]byte, error) {
 		//Algorithm string `json:"alg"`
 		N string `json:"n"` // Modulus
 		E string `json:"e"` // Public exponent
-		// D // Private exponent
 	}{
 		KeyType: "RSA",
-		//Algorithm: // TODO: eg RS256. Sure we extrat this in http-log
+		//Algorithm: // TODO: eg RS256. looks like we'll have to derive this string ourselves
 		N: base64.RawURLEncoding.EncodeToString(k.N.Bytes()),
 		E: base64.RawURLEncoding.EncodeToString(bufE),
+	})
+}
+func (k *printableRsaPrivateKey) MarshalJSON() ([]byte, error) {
+	bufE := make([]byte, 4)
+	binary.LittleEndian.PutUint32(bufE, uint32(k.E))
+	bufE = bufE[:3] // TODO: JWK spec says nothing about truncating this, check RSA spec.
+	return json.Marshal(&struct {
+		KeyType string `json:"kty"`
+		//Algorithm string `json:"alg"`
+		N string `json:"n"` // Modulus
+		E string `json:"e"` // Public exponent
+		D string `json:"d"` // Private exponent
+		// Primes - mandatory?
+		// Pre-computed values to speed stuff up. Mandatory in a JWK?
+		// Dp
+		// Dq
+		// Qinv
+		// Q - emitted by npm "pem-jwk" but not in the go struct
+	}{
+		KeyType: "RSA",
+		//Algorithm: // TODO: eg RS256. looks like we'll have to derive this string ourselves
+		N: base64.RawURLEncoding.EncodeToString(k.N.Bytes()),
+		E: base64.RawURLEncoding.EncodeToString(bufE),
+		D: base64.RawURLEncoding.EncodeToString(k.D.Bytes()),
 	})
 }
 
@@ -142,8 +207,26 @@ func (k *printableEcdsaPublicKey) MarshalJSON() ([]byte, error) {
 		Y:       base64.RawURLEncoding.EncodeToString(k.Y.Bytes()),
 	})
 }
+func (k *printableEcdsaPrivateKey) MarshalJSON() ([]byte, error) {
+	return json.Marshal(&struct {
+		KeyType string `json:"kty"`
+		Curve   string `json:"crv"`
+		X       string `json:"x"`
+		Y       string `json:"y"`
+		D       string `json:"d"`
+	}{
+		KeyType: "EC",
+		Curve:   k.Curve.Params().Name,
+		X:       base64.RawURLEncoding.EncodeToString(k.X.Bytes()),
+		Y:       base64.RawURLEncoding.EncodeToString(k.Y.Bytes()),
+		D:       base64.RawURLEncoding.EncodeToString(k.D.Bytes()),
+	})
+}
 
 func (k printableEd25519PublicKey) MarshalJSON() ([]byte, error) {
+	return nil, nil
+}
+func (k printableEd25519PrivateKey) MarshalJSON() ([]byte, error) {
 	return nil, nil
 }
 
